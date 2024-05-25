b2dbf3c2f01e59a9fd5e305888dea4a9c446582e
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-25 14:40:40 -0300

    Create prog1.php

---
+++ capitulo03/exemplos/prog1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Primeiro Programa PHP</title>
+   <style>
+      h2 {
+         text-align: center;
+      }
+   </style>
+</head>
+<body>
+   <?php 
+      // Legal, meu primeiro programa em PHP
+      echo"<h2>Primeiro programa em PHP</h2>";
+   ?>
+</body>
+</html>
