db1757f4f979f698c77f2ffe41082bb00a0431f5
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-15 14:17:14 -0300

    Create prog5.php

---
+++ capitulo03/exemplos/prog5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Aspas Simples</title>
+   <style>
+      p {
+         text-align: center;
+      }
+   </style>
+</head>
+<body>
+      <?php 
+         echo('<p>
+            Isto é um teste 
+            estou aprendendo a escrever entre aspas simples no PHP.
+         </p>');
+      ?>
+</body>
+</html>
