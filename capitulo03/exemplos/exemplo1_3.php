3ac780b58641c671bb1efe927f5515a61538ccf1
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-15 14:16:56 -0300

    Create exemplo1_3.php

---
+++ capitulo03/exemplos/exemplo1_3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Constante</title>
+</head>
+<body>
+   <?php 
+      // constante
+      define ("meuNome", "Juliano");
+      define("peso", 78);
+      
+      echo "meu nome é ". meuNome;
+      echo "<br>";
+      echo "meu peso é ". peso . " quilos";
+   ?>
+</body>
+</html>
