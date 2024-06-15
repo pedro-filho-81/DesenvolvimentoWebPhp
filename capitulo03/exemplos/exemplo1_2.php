66a0620cb7020a4f4bcc23652cec99d4065615c6
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-15 14:16:51 -0300

    Create exemplo1_2.php

---
+++ capitulo03/exemplos/exemplo1_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Usando crase</title>
+   <style>
+      * {
+         text-align: center;
+      }
+   </style>
+</head>
+<body>
+   <h1>Usando crase</h1>
+   <p>
+      Usando crase no lugar das aspas
+   </p> 
+   <?php
+      echo"<p>Texto dentro da crase</p>;"
+   ?>
+</body>
+</html>
